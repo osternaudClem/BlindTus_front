fcd71a2f3147384e5140a25969cd6abb8ce1f073
Clement Osternaud <osternaud.clement@gmail.com>
2022-10-04 12:41:57 +0200

    Add production endpoint

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
   // API
   api: {
     development: 'http://localhost:4000/api',
+    production: 'http://blindtus.cl3tus.com/api',
     socket: 'http://localhost:4001',
   },
   tmdb: {
